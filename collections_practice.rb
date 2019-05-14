edbfb973677eaf923fb003d6a73e17d300385566
Avitosh Totaram <a.totaram@gmail.com>
2019-05-14 13:21:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
 # your code goes here
+
+def begins_with_r(array)
+  def comp_value = array.find_all {|i| array[0] == "r"}
+  end
+end
+if comp_value.length = array.length
+    return "they all begin with the letter r"
+  else
+    return "it doesnt all beging with r"
+  end
+end
